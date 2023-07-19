93f51443f8fc361a176b6c0c94f6781de3109999
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2023-07-18 23:37:46 -0300

    feature: add HTML structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\Web\PagesController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,21 +15,13 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [PagesController::class, 'welcome'])->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [PagesController::class, 'dashboard'])->name('dashboard');
+    Route::get('/chat', [PagesController::class, 'chat'])->name('chat');
 });
